c6cb5843525ab52443142a2682a14dc0cb32a0e4
xuan-659 <953579022@qq.com>
2022-06-03 23:19:00 +0800

    修复bug

--- src/utlis/ajax.ts
+++ src/utlis/ajax.ts
@@ -1,49 +1,55 @@
-import { SESSION_ID_KEY } from '@/common/constants';
-import axios from 'axios';
-import Storage from './localStorage';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { catchException } from './error';
-import { formData } from './form-data';
+import { SESSION_ID_KEY } from "@/common/constants";
+import axios from "axios";
+import Storage from "./localStorage";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { catchException } from "./error";
+import { formData } from "./form-data";
 
 const storage = new Storage();
 // const baseURL = 'http://47.114.146.52:8080';
 // const baseURL = 'http://127.0.0.1:4523/mock/949932'
-const baseURL = 'http://39.101.197.160:8081/njupt-compose-paper-system'
+const baseURL = "http://39.101.197.160:8081/njupt-compose-paper-system";
 
 axios.interceptors.request.use(
-    config => {
+    (config) => {
         const sessionId = storage.get(SESSION_ID_KEY);
-        if(config.method == 'post'){
+        if (config.method == "post") {
             config.params = {};
         }
-        if(sessionId) {
+        if (sessionId) {
             config.headers.token = sessionId;
         }
         return config;
     },
-    error => {
+    (error) => {
         // TODO: 错误处理
     }
-)
+);
 
 export default class AJAX {
-
     @catchException(true)
     public async get<T>(url: string, data?: any, config?: AxiosRequestConfig) {
-    
-        const mergedConfig = { ...config, params: data};
-        return await axios.get<T>( baseURL + url, mergedConfig);
+        const mergedConfig = { ...config, params: data };
+        return await axios.get<T>(baseURL + url, mergedConfig);
     }
 
     @catchException(true)
     public async post<T>(url: string, data: any, config?: AxiosRequestConfig) {
-        if(url=='/knowledge/save' || url=='/ability/save'||'/knowledge/remove'||'/ability/remove'){
-            const mergedConfig = { ...config, params: data};
-        return await axios.post<T>( baseURL + url, data, mergedConfig);
-        }
-        else{
-            const mergedConfig = { ...config, params: formData(data)};
-            return await axios.post<T>( baseURL + url, formData(data), mergedConfig);
+        if (
+            url == "/knowledge/save" ||
+            url == "/ability/save" ||
+            url == "/knowledge/remove" ||
+            url == "/ability/remove"
+        ) {
+            const mergedConfig = { ...config, params: data };
+            return await axios.post<T>(baseURL + url, data, mergedConfig);
+        } else {
+            const mergedConfig = { ...config, params: formData(data) };
+            return await axios.post<T>(
+                baseURL + url,
+                formData(data),
+                mergedConfig
+            );
         }
     }
 }
